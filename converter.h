14fc630912bb87184790313d5f74bab336fc525a
Futile-Sudo <vasyabond23@gmail.com>
2026-02-05 15:46:11 +0300

    second commit

---
+++ converter.h
@@ -0,0 +1,9 @@
+#ifndef CONVERTER_H
+#define CONVERTER_H
+
+int charToHex(char c);
+int convertHexFileToBin(const char* input, const char* output);
+int convertBinFileToHex(const char* input, const char* output);
+
+
+#endif // COVNERTER_H
